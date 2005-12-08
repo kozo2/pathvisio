a72353b892d79ba2ad2b925ace03398db79443a1
ogo <ogo@4f21837e-9f06-0410-ae49-bac5c3a7b9b6>
2005-12-08 14:26:12 +0000

    Ik heb nu RectangleConnecter.java aangepast, lijnen die nergen heen gaan gaan nu naar een ander array, alleen dat array wil niet helemaal werken.

--- Graphics/RectangleConnecter.java
+++ Graphics/RectangleConnecter.java
@@ -11,8 +11,12 @@ public class RectangleConnecter {
 
 	public static void main(String[] args) {
 	ConnectionCheck connectioncheck= new ConnectionCheck();
+	connectioncheck.checkconnection();
 	for (int i=0 ; i<connectioncheck.connectors.length ; i++) {
-	System.out.println(" van blok " + connectioncheck.connectors[i][0]+ " naar blok " + connectioncheck.connectors[i][1]);
+		System.out.println(" van blok " + connectioncheck.connectors[i][0]+ " naar blok " + connectioncheck.connectors[i][1]);
+		}
+	for (int i=0 ; i<connectioncheck.no_connectors.length ; i++) {
+		System.out.println(" van blok " + connectioncheck.no_connectors[i][0] + " loopt een lijn naar punt " + connectioncheck.no_connectors[i][1] + ", " + connectioncheck.no_connectors[i][2]);
 		}
 	}
 }
@@ -25,6 +29,8 @@ class ConnectionCheck{
 	int[][][] linepoints={{{13,13},{38,55}},{{13,13},{40,90}},{{38,55},{40,90}},{{40,90},{75,68}},{{120,25},{38,55}},{{75,68},{13,13}},{{120,25},{160,25}}};
 	// connectors will store the rectangles that are connected by a set of points
 	int[][] connectors=new int[linepoints.length][2];
+	int[][] rect_points=new int[(linepoints.length)*2][2];	
+	int[][] no_connectors;
 	
 	public void checkconnection(){
 		// finds the blocks that are connected by the line trhough two points
@@ -33,17 +39,48 @@ class ConnectionCheck{
 				// a temporary rectangle temprectj is created
 				Rectangle temprectj=new Rectangle(rectangles[j][0],rectangles[j][1],rectangles[j][2],rectangles[j][3]);
 				if (temprectj.contains(linepoints[i][0][0], linepoints[i][0][1])) {
+					rect_points[i][0]=i+1;
+					rect_points[i][1]=j+1;
 					for (int k = 0; k < rectangles.length; k++) {
 						Rectangle temprectk=new Rectangle(rectangles[k][0],rectangles[k][1],rectangles[k][2],rectangles[k][3]);
 						if (k!=j && temprectk.contains(linepoints[i][1][0],linepoints[i][1][1])) {
-							connectors[i][0]=j;
-							connectors[i][1]=k;
+							connectors[i][0]=j+1;
+							connectors[i][1]=k+1;
 							}//close if
 						}//close for k
 					}//close if
 				}//close for j
-			}//close for i
-		//return connectors;
+			}//close for
+		int no_connection = 0;
+		int n = 0;
+		int m = 0;
+		for (int i = 0; i < connectors.length; i++) {
+			if (connectors[i][0]==0 || connectors[i][1]==0) {
+				no_connection = no_connection+1;
+				}
+			}
+		int[][] newconnectors = new int[connectors.length-no_connection][2];
+		int[][] no_connectors = new int[no_connection][3];
+		for (int i = 0; i <newconnectors.length; i++) {
+			if (connectors[i][0]!=0 && connectors[i][1]!=0) {
+				newconnectors[n][0]=connectors[i][0];
+				newconnectors[n][1]=connectors[i][1];
+				n = n+1;
+				}
+			else if (connectors[i][0]==0) {
+					no_connectors[m][0]=connectors[i][1];
+					no_connectors[m][1]=linepoints[i][0][0];
+					no_connectors[m][2]=linepoints[i][0][1];
+					m=m+1;
+					}
+			else if (connectors[i][1]==0) {
+					no_connectors[m][0]=connectors[i][0];
+					no_connectors[m][1]=linepoints[i][1][0];
+					no_connectors[m][2]=linepoints[i][1][1];
+					m=m+1;
+					}
+			}
+		connectors=newconnectors;
 	}//close checkconnection
 }
 
