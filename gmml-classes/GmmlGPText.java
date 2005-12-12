e30141be83370c96b7cc3f0d2d9bfcf60d005480
ogo <ogo@4f21837e-9f06-0410-ae49-bac5c3a7b9b6>
2005-12-12 13:58:25 +0000

    Irene: rectText toegevoegd

---
+++ gmml-classes/GmmlGPText.java
@@ -0,0 +1,9 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.applet.Applet;
+import java.awt.image.*;
+import java.awt.geom.*;
+import javax.swing.JPanel;
+
+public class GmmlGPText {
+	
