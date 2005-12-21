241112c0804ed919c5529be9872b5ae3386a453d
hakm <hakm@4f21837e-9f06-0410-ae49-bac5c3a7b9b6>
2005-12-21 10:18:13 +0000

    First working prototype for the GmmlWriter.java

---
+++ gmml-classes/GmmlWriter.java
@@ -0,0 +1,74 @@
+import org.jdom.JDOMException;
+import org.jdom.input.*;
+import org.jdom.output.*;
+import org.jdom.output.XMLOutputter;
+import org.jdom.*;
+import org.jdom.Element;
+import org.jdom.Attribute;
+import java.io.IOException;
+import java.util.*;
+import java.io.FileWriter;
+import java.io.File;
+
+public class GmmlWriter {
+
+	GmmlPathway pathway;
+	Document doc;
+	Element root;
+	
+	public GmmlWriter(GmmlPathway output) {
+		pathway = output;
+		buildDoc();
+	}
+	
+	public void dumpToScreen() {
+		try {
+			XMLOutputter screendump = new XMLOutputter(Format.getPrettyFormat());
+			File file = new File("test.xml");
+			FileWriter writer = new FileWriter(file);
+	     	screendump.output(doc, writer);
+   	}
+    	catch (IOException e) {
+      	System.err.println(e);
+    	}
+   } 
+   
+   public void buildDoc() {
+   	System.out.println("Building document...");
+   	root = new Element("Pathway");
+	   for (int i = 0; i < pathway.attributes.length; i++) {
+	   	root.setAttribute(pathway.attributes[i][0], pathway.attributes[i][1]);
+	   }
+	   Element graphics = new Element("Graphics");
+	   graphics.setAttribute("BoardHeight",Integer.toString(pathway.size[1]));
+	   graphics.setAttribute("BoardWidth",Integer.toString(pathway.size[0]));
+	   root.addContent(graphics);
+	   fillRootElement();
+	   doc = new Document(root);
+	}
+   public void fillRootElement () {
+   	for (int i = 0; i < pathway.rects.length; i++) {
+	   	Element geneproduct = new Element("GeneProduct");
+		   
+		   Element graphics = new Element("Graphics");
+		   
+		   double w, h, x, y, cx, cy;
+		   
+		   w = pathway.rects[i].getWidth();
+		   h = pathway.rects[i].getHeight();
+	   	x = pathway.rects[i].getX();
+		   y = pathway.rects[i].getY();
+		   cx = x + w/2;
+		   cy = y + h/2;
+		   
+		   graphics.setAttribute("CenterX",Integer.toString((int)cx));
+		   graphics.setAttribute("CenterY",Integer.toString((int)cy));
+		   graphics.setAttribute("Width",Integer.toString((int)w));
+		   graphics.setAttribute("Height",Integer.toString((int)h));
+		   
+		   geneproduct.addContent(graphics);
+		   root.addContent(geneproduct);
+	   }
+	}
+
+}
