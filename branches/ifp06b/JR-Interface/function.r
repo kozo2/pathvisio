ffa28c4b8acf1f00b15d69cf21d692a4669b6304
colin <colin@4f21837e-9f06-0410-ae49-bac5c3a7b9b6>
2007-01-09 14:32:30 +0000

    Several functions for loading pathway,gene database and expression dataset

--- branches/ifp06b/JR-Interface/function.r
+++ branches/ifp06b/JR-Interface/function.r
@@ -1,14 +1,30 @@
 # run_pv.r: runs PathVisio from R environment
 
-showPath <- function(y1) {
+openPathway <- function(y1) {
 
 # load library and set working directory
 library("rJava")
-setwd("D:/Project BioInformatica/SVN/JR-Interface")
+setwd("D:/Project BioInformatica/SVN")
 
 # initialize jvm
 # loading classpath's : pathvisio_v2.jar, jface.jar, swt-win32.jar, swt-win32-lib.jar
-.jinit(classpath = "D:/Project BioInformatica/SVN/pathvisio_v2.jar; D:/Project BioInformatica/SVN/lib/org.eclipse.jface.jar; D:/Project BioInformatica/SVN/lib/swt-win32.jar; D:/Work/BI/SVN/lib/swt-win32-lib.jar")
+base <- "D:\\Project BioInformatica\\SVN\\lib\\"
+classpath = paste (base, c(
+	"swt-win32.jar",
+	"pathvisio_v2.jar",
+	"jdom.jar",
+	"JRI.jar",
+	"org.eclipse.core.commands.jar",
+	"org.eclipse.equinox.common.jar",
+	"org.eclipse.jface.jar",
+	"derby.jar",
+	"junit.jar",
+	"BrowserLauncher.jar",
+	"resources.jar",
+	"R-resources.jar"
+), sep="");
+
+.jinit(classpath)
 
 # create window object
 # gmmlVisionWindow window = new gmmlVisionWindow();
@@ -32,7 +48,7 @@ window <- .jcall("gmmlVision/GmmlVision", "LgmmlVision/GmmlVisionWindow;", "getW
 }
 
 
-showGen <- function(y1,y2) {
+openGen <- function(y1,y2) {
 
 # load library and set working directory
 library("rJava")
@@ -40,7 +56,23 @@ setwd("D:/Project BioInformatica/SVN")
 
 # initialize jvm
 # loading classpath's : pathvisio_v2.jar, jface.jar, swt-win32.jar, swt-win32-lib.jar
-.jinit(classpath = "D:/Project BioInformatica/SVN/pathvisio_v2.jar; D:/Project BioInformatica/SVN/lib/org.eclipse.jface.jar; D:/Project BioInformatica/SVN/lib/swt-win32.jar; D:/Work/BI/SVN/lib/swt-win32-lib.jar")
+base <- "D:\\Project BioInformatica\\SVN\\lib\\"
+classpath = paste (base, c(
+	"swt-win32.jar",
+	"pathvisio_v2.jar",
+	"jdom.jar",
+	"JRI.jar",
+	"org.eclipse.core.commands.jar",
+	"org.eclipse.equinox.common.jar",
+	"org.eclipse.jface.jar",
+	"derby.jar",
+	"junit.jar",
+	"BrowserLauncher.jar",
+	"resources.jar",
+	"R-resources.jar"
+), sep="");
+
+.jinit(classpath)
 
 # create window object
 # gmmlVisionWindow window = new gmmlVisionWindow();
@@ -67,21 +99,55 @@ window <- .jcall("gmmlVision/GmmlVision", "LgmmlVision/GmmlVisionWindow;", "getW
 }
 
 
+openExpr <- function(y1,y2,y3) {
+
+# load library and set working directory
+library("rJava")
+setwd("D:/Project BioInformatica/SVN")
+
+# initialize jvm
+# loading classpath's : pathvisio_v2.jar, jface.jar, swt-win32.jar, swt-win32-lib.jar
+base <- "D:\\Project BioInformatica\\SVN\\lib\\"
+classpath = paste (base, c(
+	"swt-win32.jar",
+	"pathvisio_v2.jar",
+	"jdom.jar",
+	"JRI.jar",
+	"org.eclipse.core.commands.jar",
+	"org.eclipse.equinox.common.jar",
+	"org.eclipse.jface.jar",
+	"derby.jar",
+	"junit.jar",
+	"BrowserLauncher.jar",
+	"resources.jar",
+	"R-resources.jar"
+), sep="");
+
+.jinit(classpath)
+
+# create window object
+# gmmlVisionWindow window = new gmmlVisionWindow();
+window <- .jcall("gmmlVision/GmmlVision", "LgmmlVision/GmmlVisionWindow;", "getWindow");
+
+# Initiates some objects used by the program
+# initiate()
+.jcall("gmmlVision/GmmlVisionMain", "V", "initiate");
+
+# pass control ot the program to the window
+# window.setBlockOnOpen(true);
+.jcall(window, "V", "setBlockOnOpen", TRUE);
+
+# pass data file with pathway to use to the window so it will be shown when the window opens.
+# window.setPWF("D:\\Work\\BI\\SVN\\testData\\test.gpml");
+.jcall(window, "V", "setPWF", "C:\\Documents and Settings\\s040778\\Pathvisio-Data\\pathways\\MAPPs\\Rn_Contributed_20060824\\cellular_process-GenMAPP\\Rn_Apoptosis.gpml");
+
+# pass data file with gene database to use to the window
+.jcall(window, "V", "setDbName", "C:\\Documents and Settings\\s040778\\Pathvisio-Data\\gene databases\\Rn_39_34i.pgdb");
 
 # Select data file with expression dataset
-#.jcall(window, "V", "setExName", "C:\\Documents and Settings\\s040778\\Pathvisio-Data\\expression datasets\\expr_genmapp_format.pgex");
+.jcall(window, "V", "setExName", "C:\\Documents and Settings\\s040778\\Pathvisio-Data\\expression datasets\\expr_genmapp_format.pgex");
 
 # open window 
 # window.open();
-#.jcall(window, "I", "open");
-
-
-
-
-
-
-
-
-
-
-
+.jcall(window, "I", "open");
+}
