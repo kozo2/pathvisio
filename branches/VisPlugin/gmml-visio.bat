db16a324de23194f636fd54448e10b0972656e47
martijn <martijn@4f21837e-9f06-0410-ae49-bac5c3a7b9b6>
2007-04-24 11:13:42 +0000

    Closed unused brances

--- branches/VisPlugin/gmml-visio.bat
+++
@@ -1,3 +0,0 @@
-SET MY_CLASS_PATH="lib/JRI.jar";"lib/org.eclipse.equinox.common.jar";"lib/org.eclipse.equinox.supplement.jar";"lib/org.eclipse.jface.jar";"lib/swt-win32.jar";"lib/org.eclipse.core.commands.jar";"lib/jdom.jar";build;"lib/hsqldb.jar";"lib/swt-win32-lib.jar";"lib/resources.jar"
-
-java -cp %MY_CLASS_PATH% gmmlVision.GmmlVisionMain
--- branches/ifp06b/src/core/data/GmmlListener.java
+++
@@ -1,8 +0,0 @@
-package data;
-
-import java.util.EventListener;
-
-public interface GmmlListener extends EventListener {	
-	public void gmmlObjectModified(GmmlEvent e);
-	
-}
