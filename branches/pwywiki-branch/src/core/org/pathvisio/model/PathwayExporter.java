f12862e119ec2e7fb4c7a6b8767e111a4aedd92f
thomas <thomas@4f21837e-9f06-0410-ae49-bac5c3a7b9b6>
2007-08-20 14:27:20 +0000

    removed old wikipathways branch

--- branches/pwywiki-branch/src/core/org/pathvisio/model/PathwayExporter.java
+++
@@ -1,9 +0,0 @@
-package org.pathvisio.model;
-
-import java.io.File;
-
-public interface PathwayExporter {
-	public String getName();
-	public String[] getExtensions();
-	public void doExport(File file, Pathway pathway) throws ConverterException;
-}
