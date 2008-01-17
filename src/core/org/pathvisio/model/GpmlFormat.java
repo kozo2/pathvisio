511ca753958b77dc2744f3efd29738cbb1a4f22d
thomas <thomas@4f21837e-9f06-0410-ae49-bac5c3a7b9b6>
2008-01-17 15:51:17 +0000

    applied fixes from trunk r1431

--- src/core/org/pathvisio/model/GpmlFormat.java
+++ src/core/org/pathvisio/model/GpmlFormat.java
@@ -652,6 +652,10 @@ public class GpmlFormat implements PathwayImporter, PathwayExporter
 		if((id == null || id.equals("")) && o.getParent() != null) 
 			{id = o.getParent().getUniqueId();}
 		o.setGroupId (id);
+		
+		//GraphId
+		mapGraphId(o, e);
+		
 		//Style
 		o.setGroupStyle(GroupStyle.fromGpmlName(getAttribute("Group", "Style", e)));
 		//Label
@@ -664,6 +668,10 @@ public class GpmlFormat implements PathwayImporter, PathwayExporter
 		String id = o.createGroupId();
 		if (id != null && !id.equals(""))
 			{e.setAttribute("GroupId", o.createGroupId());}
+		
+		//GraphId
+		updateGraphId(o, e);
+		
 		//Style
 		setAttribute("Group", "Style", e, GroupStyle.toGpmlName(o.getGroupStyle()));
 		//Label
