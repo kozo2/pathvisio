2ba132eeebd66c141581bc1f9ed09fa6c51241ac
thomas <thomas@4f21837e-9f06-0410-ae49-bac5c3a7b9b6>
2008-11-14 13:00:14 +0000

    tag for M8.0

---
+++ tags/milestone_8.0/src/test/org/pathvisio/data/Test.java
@@ -0,0 +1,211 @@
+// PathVisio,
+// a tool for data visualization and analysis using Biological Pathways
+// Copyright 2006-2007 BiGCaT Bioinformatics
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); 
+// you may not use this file except in compliance with the License. 
+// You may obtain a copy of the License at 
+// 
+// http://www.apache.org/licenses/LICENSE-2.0 
+//  
+// Unless required by applicable law or agreed to in writing, software 
+// distributed under the License is distributed on an "AS IS" BASIS, 
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+// See the License for the specific language governing permissions and 
+// limitations under the License.
+//
+package org.pathvisio.data;
+
+import java.io.File;
+import java.io.IOException;
+import java.sql.SQLException;
+
+import junit.framework.TestCase;
+
+import org.pathvisio.Engine;
+import org.pathvisio.data.GdbManager.GdbEventListener;
+import org.pathvisio.model.DataSource;
+import org.pathvisio.model.Xref;
+
+public class Test extends TestCase implements GdbEventListener
+{	
+	//TODO
+	static final String gdbHuman = 
+		System.getProperty ("user.home") + File.separator + 
+		"PathVisio-Data/gene databases/Hs_Derby_20080102.pgdb";
+	static final String gdbRat = 
+		System.getProperty ("user.home") + File.separator + 
+		"PathVisio-Data/gene databases/Rn_Derby_20080102.pgdb";
+
+	boolean eventReceived = false;
+
+	public void gdbEvent (GdbEvent e)
+	{
+		if (e.getType() == GdbEvent.GDB_CONNECTED)
+		{
+			eventReceived = true;
+		}
+	}
+	
+	public void testGdbConnect() throws DataException
+	{
+		//TODO: create test pgdb
+		// suitable for mapping Hs_Apoptosis genes.
+		
+		assertTrue (new File (gdbHuman).exists()); // if gdb can't be found, rest of test doesn't make sense. 
+		SimpleGdb gdb = SimpleGdbFactory.createInstance (gdbHuman, new DataDerby(), 0);
+		gdb.close();
+		
+		// assertTrue (eventReceived);
+		// test reception of event...
+	}
+	
+	public void testImportSimple() throws IOException, DataException
+	{
+		Engine.init();
+		ImportInformation info = new ImportInformation();
+		File f = new File ("example-data/sample_data_1.txt");
+		assertTrue (f.exists());
+		info.setTxtFile(f);
+		String dbFileName = System.getProperty("java.io.tmpdir") + File.separator + "tempgex2";
+		info.setDbName(dbFileName);
+		SimpleGdb gdb = SimpleGdbFactory.createInstance(gdbHuman, new DataDerby(), 0);
+		GexTxtImporter.importFromTxt(info, null, gdb, GexManager.getCurrent());
+		
+		// no errors if all genes could be looked up.
+		assertEquals (info.getErrorList().size(), 0);
+		
+		ImportInformation info2 = new ImportInformation();
+		info2.setTxtFile(f);
+		dbFileName = System.getProperty("java.io.tmpdir") + File.separator + "tempgex3";
+		info2.setDbName(dbFileName);
+		
+		gdb = SimpleGdbFactory.createInstance (gdbRat, new DataDerby(), 0);
+		GexTxtImporter.importFromTxt(info, null, gdb, GexManager.getCurrent());
+		
+		// 91 errors expected if no genes can be looked up.
+		assertEquals (info.getErrorList().size(), 91);	
+	}
+
+	public void testImportAffy() throws IOException, DataException
+	{
+		Engine.init();
+		ImportInformation info = new ImportInformation();
+		File f = new File ("example-data/sample_affymetrix.txt");
+		assertTrue (f.exists());
+		info.setTxtFile(f);
+		info.guessSettings();
+		
+		assertEquals (info.getDataSource(), DataSource.AFFY);
+		assertFalse (info.getSyscodeColumn());
+		assertTrue (info.digitIsDot());
+		assertEquals (info.getIdColumn(), 0);
+		
+		String dbFileName = System.getProperty("java.io.tmpdir") + File.separator + "tempgex2";
+		info.setDbName(dbFileName);
+		info.setSyscodeColumn(false);
+		info.setDataSource(DataSource.AFFY);
+		SimpleGdb gdb = SimpleGdbFactory.createInstance (gdbRat, new DataDerby(), 0);
+		GexTxtImporter.importFromTxt(info, null, gdb, GexManager.getCurrent());
+		
+		// just 6 errors if all goes well
+		assertEquals (info.getErrorList().size(), 6);		
+	}
+
+	/**
+	 * Column headers have lenghts of over 50.
+	 * Make sure this doesn't lead to problems when setting sample names
+	 */
+	public void testImportLongHeaders() throws IOException, DataException
+	{
+		Engine.init();
+		ImportInformation info = new ImportInformation();
+		File f = new File ("example-data/sample_data_long_headers.txt");
+		assertTrue (f.exists());
+		info.setTxtFile(f);
+		info.guessSettings();
+		
+		assertEquals (info.getDataSource(), DataSource.ENTREZ_GENE);
+		assertFalse (info.getSyscodeColumn());
+		assertTrue (info.digitIsDot());
+		assertEquals (info.getIdColumn(), 0);
+		
+		String dbFileName = System.getProperty("java.io.tmpdir") + File.separator + "tempgex3";
+		info.setDbName(dbFileName);
+		
+		SimpleGdb gdb = SimpleGdbFactory.createInstance(gdbHuman, new DataDerby(), 0);
+		GexTxtImporter.importFromTxt(info, null, gdb, GexManager.getCurrent());
+		
+		// 0 errors if all goes well
+		assertEquals (info.getErrorList().size(), 0);		
+	}
+
+	/**
+	 * Test dataset contains two columns with textual data
+	 * make sure this doesn't give problems during import
+	 */
+	public void testImportWithText() throws IOException, DataException
+	{
+		Engine.init();
+		ImportInformation info = new ImportInformation();
+		File f = new File ("example-data/sample_data_with_text.txt");
+		assertTrue (f.exists());
+		info.setTxtFile(f);
+		info.guessSettings();
+		
+		assertEquals (info.getDataSource(), DataSource.ENTREZ_GENE);
+		assertTrue (info.getSyscodeColumn());
+		assertEquals (info.getCodeColumn(), 1);
+		assertTrue (info.digitIsDot());
+		assertEquals (info.getIdColumn(), 0);
+		
+		String dbFileName = System.getProperty("java.io.tmpdir") + File.separator + "tempgex4";
+		info.setDbName(dbFileName);
+		
+		SimpleGdb gdb = SimpleGdbFactory.createInstance(gdbHuman, new DataDerby(), 0);
+		GexTxtImporter.importFromTxt(info, null, gdb, GexManager.getCurrent());
+		
+		// 0 errors if all goes well
+		assertEquals (info.getErrorList().size(), 0);		
+	}
+
+	public void gexHelper(DBConnector con, String filename) throws DataException, SQLException
+	{
+		String dbFileName = System.getProperty("java.io.tmpdir") + File.separator + filename;
+
+		// TODO: check if filename gets .pgex or .pgdb?
+		SimpleGex sgex = new SimpleGex (dbFileName, true, con);
+		
+		assertTrue (new File(dbFileName).exists());
+		
+		sgex.prepare();
+		sgex.addSample(55, "mysample", 99);
+		sgex.addExpr(new Xref ("abc_at", DataSource.AFFY), "55", "3.141", 77);
+		
+		// TODO: this is messy. call finalize on writeable db, not close...
+		sgex.finalize();
+		
+		// read data back
+		sgex = new SimpleGex (dbFileName, false, con);
+		
+		Sample s = sgex.getSample(55);
+		assertEquals (s.getName(), "mysample");
+		assertEquals (s.getDataType(), 99);
+		
+		//TODO: test data value as well.
+		
+		sgex.close();
+	}
+	
+	public void testGexDerby() throws DataException, SQLException
+	{
+		gexHelper (new DataDerby(), "tempgex1a");
+	}
+
+	//TODO: re-enable
+	public void disabled_testGexDirectory() throws DataException, SQLException
+	{
+		gexHelper (new DataDerbyDirectory(), "tempgex1b");
+	}
+
+}
---
+++ tags/milestone_8.0/tools/lucene-indexer/src/org/pathvisio/indexer/PathwayIndexer.java
@@ -0,0 +1,125 @@
+// PathVisio,
+// a tool for data visualization and analysis using Biological Pathways
+// Copyright 2006-2007 BiGCaT Bioinformatics
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); 
+// you may not use this file except in compliance with the License. 
+// You may obtain a copy of the License at 
+// 
+// http://www.apache.org/licenses/LICENSE-2.0 
+//  
+// Unless required by applicable law or agreed to in writing, software 
+// distributed under the License is distributed on an "AS IS" BASIS, 
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+// See the License for the specific language governing permissions and 
+// limitations under the License.
+//
+package org.pathvisio.indexer;
+
+import java.io.IOException;
+
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field;
+import org.apache.lucene.index.CorruptIndexException;
+import org.apache.lucene.index.IndexWriter;
+import org.pathvisio.model.Pathway;
+import org.pathvisio.model.PathwayElement;
+import org.pathvisio.wikipathways.WikiPathways;
+
+/**
+ * Class that indexes several metadata for a pathway.
+ * @author thomas
+ *
+ */
+public class PathwayIndexer extends IndexerBase {
+	/**
+	 * The name of a pathway
+	 */
+	public static final String FIELD_NAME = "name";
+	/**
+	 * The organism of a pathway
+	 */
+	public static final String FIELD_ORGANISM = "organism";
+	
+	/**
+	 * The WikiPathways category a pathway belongs to
+	 */
+	public static final String FIELD_CATEGORY = "category";
+
+	/**
+	 * The WikiPathways description of a pathway
+	 */
+	public static final String FIELD_DESCRIPTION = "description";
+	
+	/**
+	 * The TextLabel attribute of an element on the pathway
+	 */
+	public static final String FIELD_TEXTLABEL = "textlabel";
+	
+	/**
+	 * Create a PathwayIndexer
+	 * @param source The source of the pathway (e.g. a file or url)
+	 * @param p The pathway to index
+	 * @param w The IndexWriter to write the index to
+	 */
+	public PathwayIndexer(String source, Pathway p, IndexWriter w) {
+		super(source, p, w);
+	}
+	
+	public void indexPathway() throws CorruptIndexException, IOException {
+		Document doc = new Document();
+		PathwayElement info = pathway.getMappInfo();
+		doc.add(
+				new Field(
+						FIELD_NAME, 
+						info.getMapInfoName() == null ? "" : info.getMapInfoName(), 
+						Field.Store.YES, 
+						Field.Index.TOKENIZED
+				)
+		);
+		doc.add(
+				new Field(
+						FIELD_ORGANISM,
+						info.getOrganism() == null ? "" : info.getOrganism(), 
+						Field.Store.YES, 
+						Field.Index.TOKENIZED
+				)
+		);
+		
+		//Process text labels
+		for(PathwayElement pe : pathway.getDataObjects()) {
+			String txt = pe.getTextLabel();
+			if(txt != null && !"".equals(txt)) {
+				doc.add(
+						new Field(
+								FIELD_TEXTLABEL,
+								txt,
+								Field.Store.YES,
+								Field.Index.TOKENIZED
+						)
+					);
+			}
+		}
+		
+		//Process comments
+		for(PathwayElement.Comment c : info.getComments()) {
+			if(WikiPathways.COMMENT_CATEGORY.equals(c)) {
+				doc.add(new Field(
+						FIELD_CATEGORY,
+						c.getComment(),
+						Field.Store.YES,
+						Field.Index.TOKENIZED
+				));
+			}
+			if(WikiPathways.COMMENT_DESCRIPTION.equals(c)) {
+				doc.add(new Field(
+						FIELD_DESCRIPTION,
+						c.getComment(),
+						Field.Store.YES,
+						Field.Index.TOKENIZED
+				));
+			}
+		}	
+		addDocument(doc);
+	}
+}
